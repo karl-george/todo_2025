22ada310be7a3df64bb40a5ec42ae11c9c0715ee
Karl George <karlgeorge87@gmail.com>
2025-02-03 10:21:57 +0100

    Add Clerk

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,26 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
+import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo';
+import { Slot } from 'expo-router';
+import { tokenCache } from '@/utils/cache';
 
-export default function RootLayout() {
-  return <Stack />;
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+if (!publishableKey) {
+  throw new Error(
+    'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env'
+  );
 }
+
+const InitialLayout = () => {
+  return <Stack />;
+};
+
+const RootLayout = () => {
+  return (
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+      <ClerkLoaded>
+        <InitialLayout />
+      </ClerkLoaded>
+    </ClerkProvider>
+  );
+};
