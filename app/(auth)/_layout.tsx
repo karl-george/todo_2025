7789e61e4f3e95e2c7120d044f5d812538e7ed15
Karl George <karlgeorge87@gmail.com>
2025-02-04 11:26:15 +0100

    Add layout file

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,10 @@
+import { Stack } from 'expo-router';
+import React from 'react';
+
+const Layout = () => {
+  return (
+    <Stack screenOptions={{ contentStyle: { backgroundColor: '#fff' } }} />
+  );
+};
+
+export default Layout;
